241dcdd15d6e8d2d9d1eb1949f048b48b8ce81aa
oeddahab@emeal.nttdata.com <oeddahab@emeal.nttdata.com>
2024-11-20 12:57:34 +0100

    Terminar de configurar el microservicio/Ejercutar varias instacias(cada una con su puerto) de un microservicio para verlas reflejadas en Eureka Server

--- src/main/java/com/nttdata/microservicio1/Microservicio1Application.java
+++ src/main/java/com/nttdata/microservicio1/Microservicio1Application.java
@@ -5,9 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Microservicio1Application {
-
+	//Para crear varias instacias de un microservicio duplicamos 
+	//el .yml y el archivo de ejecución e indicamos el puerto
 	public static void main(String[] args) {
 		SpringApplication.run(Microservicio1Application.class, args);
 	}
-
 }
